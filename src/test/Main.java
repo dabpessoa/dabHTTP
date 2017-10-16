edf467f9efd159b4c935a3e037cf38aa1f161c6f
dabpessoa <dabpessoa@gmail.com>
2017-10-15 22:18:27 -0300

    Correção para realização de POST contendo corpo no request.

--- src/test/Main.java
+++ src/test/Main.java
@@ -41,31 +41,31 @@ public class Main {
 
 //		ProxyCredentials proxyCredentials = new ProxyCredentials("proxy", 8080, "dabpessoa", "senha");
 
-		StringBuilder jsonRequestBuilder = new StringBuilder();
-		jsonRequestBuilder.append("{ ");
-		jsonRequestBuilder.append("	\"test\":false, ");
-		jsonRequestBuilder.append("	\"language\":\"en\", ");
-		jsonRequestBuilder.append("	\"command\":\"PING\", ");
-		jsonRequestBuilder.append("	\"merchant\":{ ");
-		jsonRequestBuilder.append("		\"apiLogin\":\"pRRXKOl8ikMmt9u\", ");
-		jsonRequestBuilder.append("		\"apiKey\":\"4Vj8eK4rloUd272L48hsrarnUA\" ");
-		jsonRequestBuilder.append("	} ");
-		jsonRequestBuilder.append("} ");
-
-		Map<String, List<String>> headers = new HashMap<String, List<String>>();
-		headers.put("Content-Type", Arrays.asList("application/json; charset=utf-8"));
-		headers.put("Accept", Arrays.asList(AcceptOption.JSON.getMimeTypeString()));
-		headers.put("Accept-Charset", Arrays.asList("utf-8"));
-		headers.put("Content-Length", Arrays.asList(Integer.toString(jsonRequestBuilder.toString().length())));
-
-		String url = "https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi";
-		String method = HTTPMethods.POST.name();
-
-		HTTPConnection connection = new HTTPConnection(url, method, headers);
-
-		String response = connection.request(jsonRequestBuilder.toString().getBytes(StandardCharsets.UTF_8));
-
-		System.out.println(response);
+//		StringBuilder jsonRequestBuilder = new StringBuilder();
+//		jsonRequestBuilder.append("{ ");
+//		jsonRequestBuilder.append("	\"test\":false, ");
+//		jsonRequestBuilder.append("	\"language\":\"en\", ");
+//		jsonRequestBuilder.append("	\"command\":\"PING\", ");
+//		jsonRequestBuilder.append("	\"merchant\":{ ");
+//		jsonRequestBuilder.append("		\"apiLogin\":\"pRRXKOl8ikMmt9u\", ");
+//		jsonRequestBuilder.append("		\"apiKey\":\"4Vj8eK4rloUd272L48hsrarnUA\" ");
+//		jsonRequestBuilder.append("	} ");
+//		jsonRequestBuilder.append("} ");
+//
+//		Map<String, List<String>> headers = new HashMap<String, List<String>>();
+//		headers.put("Content-Type", Arrays.asList("application/json; charset=utf-8"));
+//		headers.put("Accept", Arrays.asList(AcceptOption.JSON.getMimeTypeString()));
+//		headers.put("Accept-Charset", Arrays.asList("utf-8"));
+//		headers.put("Content-Length", Arrays.asList(Integer.toString(jsonRequestBuilder.toString().length())));
+//
+//		String url = "https://sandbox.api.payulatam.com/reports-api/4.0/service.cgi";
+//		String method = HTTPMethods.POST.name();
+//
+//		HTTPConnection connection = new HTTPConnection(url, method, headers);
+//
+//		String response = connection.request(jsonRequestBuilder.toString().getBytes(StandardCharsets.UTF_8));
+//
+//		System.out.println(response);
 
 
 	}
